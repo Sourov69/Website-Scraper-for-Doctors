9e73c0e0a49cf09d1f824e908c11d7dcb8069178
Sourovtalukder69 <sourovtalukder69@gmail.com>
2026-06-05 16:20:28 +0600

    Sample therapist_urls scraping done

--- test.py
+++ test.py
@@ -1,6 +1,24 @@
 import time
 import random
+import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from playwright.sync_api import sync_playwright
+
+
+all_therapist_url_list = []
+
+def extract_therapist_url(html_page):
+    soup = BeautifulSoup(html_page, "html.parser")
+    therapist_list  =soup.select_one('div[data-testid="best-match-cards"]')
+    base_url = "https://care.headway.co"
+
+    therapist_url_list = []
+    for i in therapist_list:
+        href = i.select_one("a.block.h-full.w-full").get("href")
+        full_url = urljoin(base_url, href)
+        therapist_url_list.append(full_url)
+    return therapist_url_list
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
@@ -10,28 +28,37 @@ with sync_playwright() as p:
     page.goto("https://care.headway.co/search/california?address=San+Diego%2C+CA+92108%2C+USA&forChild=ADULTS&frontEndCarrierId=61&issues=lgbtq&lat=32.7742488&lon=-117.1411815&medium=VIRTUAL&state=CALIFORNIA&typeOfCare=Talk+Therapy&availabilities=EVENING&modalityCareTypes=INDIVIDUAL_THERAPY")
     page.wait_for_timeout(3000)
     
-    for _ in range(1, random.randint(7, 10)):
-        scrool_amount = random.randint(300, 600)
+    for _ in range(1, random.randint(13, 15)):
+        scrool_amount = random.randint(800, 1000)
         page.mouse.wheel(0, scrool_amount)
         time.sleep(random.uniform(0.5, 2))
 
     therapist_list = page.locator('div[data-testid="best-match-cards"] > div')
     print(therapist_list.count())
+    
 
-   
-    # with context.expect_page() as therapist_profile:
-    #     therapist_list.nth(1).click()
-    # therapist_profile_page = therapist_profile.value
-    # therapist_profile_page.wait_for_timeout(3000)
- 
-    # for i in range(1, 3):
-    #     therapist_profile_page.mouse.wheel(0, 400)
-    #     therapist_profile_page.wait_for_timeout(1500)
-    # therapist_profile_page.close()
+    # while True:
+    for i in range(1, 3):
+        next_page = page.locator("nav.hlx-pagination ul > li:last-child")
+        if not next_page.is_visible():
+            break
 
-    therapist_list_html_page1 = page.content()
-    with open("page.html", "w", encoding="utf-8") as f:
-        f.write(therapist_list_html_page1)
+        next_page.click()
+        page.wait_for_timeout(300)
+        for _ in range(1, random.randint(12, 15)):
+            scrool_amount = random.randint(800, 1000)
+            page.mouse.wheel(0, scrool_amount)
+            time.sleep(random.uniform(0.5, 2))
+        html_page = page.content()
+        
+        print(extract_therapist_url(html_page))
+        all_therapist_url_list.extend(extract_therapist_url(html_page))
 
+    page.wait_for_timeout(3000)
+  
     page.wait_for_timeout(2500)
     page.close()
+
+df = pd.Series(all_therapist_url_list).reset_index()
+
+df.to_excel("therapist_url_list.xlsx", sheet_name="therapist_profile", index=False)
