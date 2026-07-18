e4f75d469cc63e6b54b9223070753d3bced9359c
Sourovtalukder69 <sourovtalukder69@gmail.com>
2026-07-18 14:19:36 +0600

    Databse connection and value insertion uisng python

--- db.py
+++ db.py
@@ -23,3 +23,24 @@ def get_connection():
 conn = get_connection()
 
 
+def insert_product(therapist_name, sub_title, Bio, therapist_url, Specialty, style):
+    conn = get_connection()
+    cusror = conn.cursor()
+
+    query = """
+            INSERT IGNORE INTO doctor_scraper.therapists_list
+            ( therapist_name, sub_title, Bio, therapist_url, Specialty, style)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+
+    values = (therapist_name, sub_title, Bio, therapist_url, Specialty, style)
+    cusror.execute(query, values)
+    conn.commit()
+    cusror.close()
+    conn.close()
+
+insert_product("Mr Sourov", "Engineer", "Hey i am sourov", "https://sourovtalukder.com", "Data Science", "Pro")
+
+
+    
+
